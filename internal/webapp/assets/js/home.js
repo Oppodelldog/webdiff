c26a627457588bb718eb71002ce856673632d8ba
Oppodelldog <odog@netcologne.de>
2022-04-30 21:55:05 +0200

    some infos

--- internal/webapp/assets/js/home.js
+++ internal/webapp/assets/js/home.js
@@ -1,8 +1,17 @@
 const componentHome = {
     template: `
       <div class="row">
-      <h6>Home</h6>
-        </div>`,
+        <h3>Webdiff </h3>
+        <p class="lead">
+            Webdiff enables you to download one or more webpages and compare them.
+        </p>
+        <p><strong>Download</strong><br/>
+        In the Download section you put in a list of urls that will be download and categorize them by typing a session name.
+        </p>
+        <p><strong>Diff</strong><br/>
+        The Diff section then lets you compare files from your recorded sessions.
+        </p>
+      </div>`,
     data() {
         return {}
     },

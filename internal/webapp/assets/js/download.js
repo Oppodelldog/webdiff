cd57d921a1fe17ab25dba5cce243f3b5638ae4f1
Oppodelldog <odog@netcologne.de>
2022-05-01 00:10:10 +0200

    add some feedback

--- internal/webapp/assets/js/download.js
+++ internal/webapp/assets/js/download.js
@@ -1,13 +1,13 @@
 const componentDownload = {
     template: `
  <div class="row">
-    <div class="input-group mb-3">
-        <div class="mb-3">
+    <div class="input-group">
+        <div>
             <label for="floatingTextarea2">Session</label>
             <input type="text" class="form-control" placeholder="New Session" aria-label="New Session"
                    v-model="selectedSession">
         </div>
-        <div class="mb-3">
+        <div class="ms-3">
             <label for="floatingTextarea2">existing sessions</label>
             <select v-model="selectedSession" class="form-select"
                     aria-label="Choose a session to load file for selection on the left side">
@@ -16,31 +16,46 @@ const componentDownload = {
             </select>
         </div>
     </div>
-    <div class="mb-3">
+</div>
+<div class="row mt-3">
+    <div class="col">
         <label for="floatingTextarea2">Urls</label>
         <textarea v-model="urls" class="form-control" placeholder="one url per line" id="urlList"
                   style="height: 200px"></textarea>
     </div>
-    <div class="row">
-
-        <div class="col">
-            <button type="button" :disabled="isFillUrlsButtonDisabled()" @click="fillUrls()" class="btn btn-outline-primary">Fill Urls from
-                existing session
-            </button>
+</div>
+<div class="row mt-3">
+    <div class="col">
+        <button type="button" :disabled="isFillUrlsButtonDisabled()" @click="fillUrls()"
+                class="btn btn-outline-primary">Fill Urls from
+            existing session
+        </button>
+    </div>
+    <div class="col">
+    </div>
+    <div class="col-auto">
+        <button type="button" class="btn btn-primary" @click="download()">Download</button>
+    </div>
+</div>
+<div class="row mt-3">
+    <div class="col">
+        <div class="alert alert-success" role="alert" v-if="downloadSuccess">
+            Downloads have been enqueued
         </div>
-        <div class="col">
-        </div>
-        <div class="col-auto">
-            <button type="button" class="btn btn-primary" @click="download()">Download</button>
+        <div class="alert alert-danger" role="alert" v-if="downloadFailure">
+            Error while enqueuing
         </div>
     </div>
 </div>
+
         `,
     data() {
         return {
             sessions: [],
             urls: "",
             selectedSession: "",
+            downloadSuccess: false,
+            downloadFailure: false,
         }
     },
     methods: {
@@ -60,10 +75,17 @@ const componentDownload = {
         async download() {
             if (this.selectedSession.length === 0) return;
             if (this.urls.length === 0) return;
+            this.downloadSuccess = false;
+            this.downloadFailure = false;
 
-            await this.urls.split("\n").forEach((url) => {
-                enqueueDownload(this.selectedSession, url)
-            })
+            try {
+                await this.urls.split("\n").forEach((url) => {
+                    enqueueDownload(this.selectedSession, url)
+                })
+                this.downloadSuccess = true;
+            } catch (e) {
+                this.downloadFailure = true;
+            }
         },
     },
     async mounted() {

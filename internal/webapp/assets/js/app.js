59f73a7a76deec6a485a04a07ef73e5c9bce4794
Oppodelldog <odog@netcologne.de>
2022-04-30 16:59:09 +0200

    add router

--- internal/webapp/assets/js/app.js
+++ internal/webapp/assets/js/app.js
@@ -1,4 +1,61 @@
-let app = Vue.createApp({
+const Home = {template: '<div><h6>Home</h6></div>'}
+const Diff = {template: '<diff/>'}
+const Download = {template: '<div><h6>Download</h6></div>'}
+
+
+const routes = [
+    {path: '/', component: Home},
+    {path: '/diff', component: Diff},
+    {path: '/download', component: Download},
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+})
+
+const app = Vue.createApp({});
+
+app.component('diff', {
+    template: `
+      <div class="row">
+            <div class="col-sm-2">
+                <select v-model="selectedSessionA" @change="loadFilesA()" class="form-select"
+                        aria-label="Choose a session to load file for selection on the left side">
+                    <option disabled value="">Choose a session</option>
+                    <option v-for="session in sessions" :value="session">{{session}}</option>
+                </select>
+                <select class="form-select" multiple
+                        aria-label="select files to diff with files selected on the right side" v-model="selectedFilesA"
+                        @change="tryDiff()">
+                    <option v-for="file in filesA" :value="file.file">{{file.file}}</option>
+                </select>
+            </div>
+            <div class="col-sm-8">
+                <div v-for="d in diffContents">
+                    <span class="badge bg-dark">
+                        {{d.session_a}}/{{d.id_a}}
+                    </span>
+                    <i class="bi bi-file-diff-fill"></i>
+                    <span class="badge bg-dark">
+                        {{d.session_b}}/{{d.id_b}}
+                    </span>
+                    <pre>{{d.diff}}</pre>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <select v-model="selectedSessionB" @change="loadFilesB()" class="form-select"
+                        aria-label="Choose a session to load file for selection on the right side">
+                    <option disabled value="">Choose a session</option>
+                    <option v-for="session in sessions" :value="session">{{session}}</option>
+                </select>
+                <select class="form-select" multiple
+                        aria-label="select files to diff with files selected on the left side" v-model="selectedFilesB"
+                        @change="tryDiff()">
+                    <option v-for="file in filesB" :value="file.file">{{file.file}}</option>
+                </select>
+            </div>
+        </div>`,
     data() {
         return {
             sessions: [],
@@ -51,4 +108,5 @@ let app = Vue.createApp({
     }
 });
 
+app.use(router)
 app.mount('#app')
